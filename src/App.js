441a3ade94b52ee73dedf3702c60f81cd4121be9
Peter <petecruckshank@gmail.com>
2018-07-27 22:43:24 -0400

    laid down basic parts of app, and got marked working

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import marked from 'marked';
 
 class App extends Component {
+  
+
+  componentWillMount() {
+    // var to hold marked.js file
+  let test = marked("This is **markdown** _text_ ");
+  console.log(test);
+  }
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <textarea name="" id="editor" cols="" rows="">Editor</textarea>
+        <div id="preview"></div>
       </div>
     );
   }
