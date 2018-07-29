d3c3a8813f8488f03b9715b514e0b3ac21ef68dd
Peter <petecruckshank@gmail.com>
2018-07-28 21:55:52 -0400

    added State and updateEditor() to scope

--- src/App.js
+++ src/App.js
@@ -3,20 +3,44 @@ import './App.css';
 import marked from 'marked';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      editorText : "Editor",
+      previewText : "<p>Hello</p></p>"
+    }
+    this.updateEditor = this.updateEditor.bind(this);
+  }
+
+  updateEditor() {
+    const _text_ = document.querySelector("#editor").value;
+    this.setState({editorText : _text_});
+    const markdown = marked(_text_);
+    this.setState({previewText: markdown});
+    const preview = document.querySelector("#preview");
+    console.log(markdown);
+    preview.innerHTML = preview;
+  }
   
 
-  componentWillMount() {
+  componentDidMount() {
     // var to hold marked.js file
   let test = marked("This is **markdown** _text_ ");
+  // test = document.querySelector('#editor').innerText;
   console.log(test);
+  test = marked(`**Wow** _I can't believe it_`);
+  document.querySelector('#preview').innerHTML = test;
   }
 
 
   render() {
+   
     return (
       <div className="App">
-        <textarea name="" id="editor" cols="" rows="">Editor</textarea>
-        <div id="preview"></div>
+        <div className="editor">
+        <textarea name="" id="editor" cols="" rows="" onChange={this.updateEditor} >{this.state.editorText}</textarea>
+        </div>
+        <div id="preview">{this.previewText}</div>
       </div>
     );
   }
