0e87835ddcd7df8dc0eec53c1b12e26bb01147b6
Peter Cruckshank <petecruckshank@gmail.com>
2018-08-01 14:27:06 -0400

    changed mainHtml to temp literal, but not parsing right

--- src/App.js
+++ src/App.js
@@ -5,27 +5,37 @@ import marked from "marked";
 class App extends Component {
   constructor(props) {
     super(props);
-    this.mainHtml = ``;
+    this.mainHtml = `
+    # Try this
+    ## Cool Editor
+    A project from [Free Code Camp](https://learn.freecodecamp.org)
+    You can use code in-line console.log("")
+    Or Code Blocks
+    * An image or something
+    **Woah bold text**
+    
+    `;
     this.state = {
-      editorText: "Editor",
-      previewText: this.mainHtml
+      editorText: this.mainHtml,
+      previewText: null
     };
     this.updateEditor = this.updateEditor.bind(this);
   }
 
   updateEditor() {
+    // get MD text from #editor and update editorText
     const _text_ = document.querySelector("#editor").value;
     this.setState({ editorText: _text_ });
+    
+    // parse the markdown and update previewText
     const markdown = marked(_text_);
-    // this.mainHtml.push(markdown);
     this.setState({ previewText: markdown  });
     // eslint-disable-next-line
-    const preview = document.querySelector("#preview");
     console.dir(markdown);
-    // preview.innerHTML = preview;
   }
 
   componentDidMount() {
+    this.updateEditor();
     // var to hold marked.js file
     let test = marked("This is **markdown** _text_ ");
     // test = document.querySelector('#editor').innerText;
